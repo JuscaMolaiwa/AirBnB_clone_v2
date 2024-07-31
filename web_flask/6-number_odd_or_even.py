1785d7f7498e02f7183cee54d0a2197708785b0b
Jusca <jusca.mol@gmail.com>
2024-07-31 22:47:36 +0000

    fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application
+Starts a Flask web application.
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -42,20 +41,20 @@ def imanumber(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
-    """Display a HTML page only if n is an integer"""
+    """Display an HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def numbersandevenness(n):
-    """Display a HTML page only if n is an integer"""
+    """Display an HTML page only if n is an integer"""
     if n % 2 == 0:
         even = 'even'
     else:
         even = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n,
-                           even=even)
+    return render_template('6-number_odd_or_even.html', n=n, even=even)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
 
